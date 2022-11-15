9b087d521759c146897e30a94efb77c5688e52d8
jkirst <joaoalekirst@gmail.com>
2022-11-15 15:15:23 -0300

    Expert Talk 15/11/2022

---
+++ src/ContaCorrente.java
@@ -0,0 +1,17 @@
+
+public class ContaCorrente extends Conta {
+
+	
+	
+
+	@Override
+	public void imprimirExtrato() {
+		System.out.println("Extrato Conta Corrente");
+		super.ImprimirInfosComuns();
+		
+	}
+	
+	
+	
+
+}
---
+++ src/Iconta.java
@@ -0,0 +1,14 @@
+
+public interface Iconta {
+
+	
+	 void sacar(double valor);
+		
+     void depositar(double valor);
+		
+	 void transferir(double valor, Conta contadestino);
+	 
+	 void imprimirExtrato();
+		
+	
+}
